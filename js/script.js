d8cd375e64bc756f43eac605b7d2b07d3658b48a
Artyom <example@gmail.com>
2022-02-27 15:31:54 +0700

    First commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+    "use strict";
+    
+    function pow(x, n) {
+        let result = 1;
+        
+        for (let i = 0; i < n; i++) {
+            result = result*x;
+        }
+        
+        return result;
+    }
+
+    console.log(pow(5, 0));
