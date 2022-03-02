27f449e1f20e7c8b814c3d73060485cb48c736d4
Artyom <example@gmail.com>
2022-03-02 17:04:50 +0700

    Practice task 1 done

--- js/script.js
+++ js/script.js
@@ -1,14 +1,40 @@
     "use strict";
     
-    function pow(x, n) {
-        let result = 1;
+    // function pow(x, n) {
+    //     let result = 1;
         
-        for (let i = 0; i < n; i++) {
-            result = result*x;
-        }
+    //     for (let i = 0; i < n; i++) {
+    //         result = result*x;
+    //     }
         
-        return result;
-    }
+    //     return result;
+    // }
 
-    console.log(pow(5, 0));
-    console.log(pow(2,2));
+    // console.log(pow(5, 0));
+    // console.log(pow(2,2));
+
+    
+    /* ----------------  Первое задание урок 12  ---------------- */
+
+
+    let numberOfFilms;
+        
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотерли?', '');
+
+    let personalMovieDB = {
+        count: numberOfFilms,
+        movies: {},
+        actors: {},
+        genres: [],
+        privat: false,
+    };
+
+    let movie = prompt('Один из последних просмотренных фильмов?',''),
+        mark = prompt('На сколько оцените его?',''),
+        movie2 = prompt('Один из последних просмотренных фильмов?',''),
+        mark2 = prompt('На сколько оцените его?','');
+
+    personalMovieDB.movies[movie] = mark;
+    personalMovieDB.movies[movie2] = mark2;
+
+    console.log(personalMovieDB);
